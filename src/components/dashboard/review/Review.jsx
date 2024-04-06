8ec373db04b863ca26a61a0e7d65a935ac9ed1d0
Taofeeq001 <taofeeqkolapo@gmail.com>
2024-04-06 06:57:52 -0700

    updating review page

--- src/components/dashboard/review/Review.jsx
+++ src/components/dashboard/review/Review.jsx
@@ -6,7 +6,7 @@ const Review = () => {
     return (
         <div className='px-[5%] py-3 bg-[var(--secondary-color)] h-[100vh]'>
             <div className='flex lg:flex-row w-full lg:justify-between flex-col'>
-                <div className='lg:w-[60%] w-full h-screen overflow-y-auto'>
+                <div className='lg:w-[60%] w-full h-[70vh] overflow-y-auto'>
                     <Reviewinfo/>
                 </div>
                 <div className='lg:w-[37%] w-full'>
--- src/components/dashboard/review/ReviewImg.jsx
+++ src/components/dashboard/review/ReviewImg.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const ReviewImg = () => {
   return (
-    <div>ReviewImg</div>
+    <div className='grid grid-cols-2 gap-3'>
+        <img src="/images/Frame 42.png" alt="" />
+        <img src="/images/Frame 43.png" alt="" />
+        <img src="/images/Frame 44.png" alt="" />
+        <img src="/images/last.png" alt="" />
+    </div>
   )
 }
 
